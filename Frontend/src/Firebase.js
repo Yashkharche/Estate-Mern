9c48fc9245699f7c429c79c212d504ac2de490ed
Yash Kharche <101669598+Yashkharche@users.noreply.github.com>
2024-02-12 18:26:02 +0530

    Update Firebase.js

--- Frontend/src/Firebase.js
+++ Frontend/src/Firebase.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  // "AIzaSyDaU961EPjKq8Np7na_sj2xiFt0IVl6beM",
   authDomain: "estate-mern-3630e.firebaseapp.com",
   projectId: "estate-mern-3630e",
   storageBucket: "estate-mern-3630e.appspot.com",
